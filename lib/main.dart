b2df4c46a2a718e1f46d4c1b12e23b8777b62d74
sys-arthur <arthurern48@gmail.com>
2022-03-21 15:27:31 -0300

    att

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'modules/ui/mobile/splashscreen/splashscreen_page.dart';
+
 void main() {
   SystemChrome.setSystemUIOverlayStyle(
-      const SystemUiOverlayStyle(statusBarColor: Colors.transparent));
+    const SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+    ),
+  );
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-  
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
